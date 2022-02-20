45e30a69bd6e5a943478ad7e38b4debfa5169d51
NicosNicolaou16 <nicosnicos16@gmail.com>
2022-02-20 17:03:09 +0200

    minor fixes

--- app/src/main/java/com/nick/samplecomposewithhiltandroom/ui/generic_compose_views/Toolbar.kt
+++ app/src/main/java/com/nick/samplecomposewithhiltandroom/ui/generic_compose_views/Toolbar.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.nick.samplecomposewithhiltandroom.ui.theme.Purple700
 
 @Composable
 fun CustomToolbar(title: Int) {
--- app/src/main/java/com/nick/samplecomposewithhiltandroom/ui/launcher_screen/launcher_screen.kt
+++ app/src/main/java/com/nick/samplecomposewithhiltandroom/ui/launcher_screen/launcher_screen.kt
@@ -2,6 +2,7 @@ package com.nick.samplecomposewithhiltandroom.ui.launcher_screen
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -10,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.nick.samplecomposewithhiltandroom.R
@@ -22,12 +24,14 @@ fun LauncherScreen(navController: NavController) {
         Animatable(0f)
     }
     LaunchedEffect(key1 = true) {
-        delay(1500L)
+        delay(2000L)
         navController.navigate(SHIPS_SCREEN)
     }
     Box(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.DarkGray)
     ) {
         Image(
             painter = painterResource(id = R.drawable.ic_baseline_image_24),
