d5fd4727b2cd415b455bcfebc2c134553bf704ed
NicosNicolaou16 <nicosnicos16@gmail.com>
2022-02-26 09:57:25 +0200

    update the Gradle to the latest version, update some dependencies libraries, fix an issue with launcher screen and update the README file

--- app/src/main/java/com/nick/samplecomposewithhiltandroom/ui/launcher_screen/launcher_screen.kt
+++ app/src/main/java/com/nick/samplecomposewithhiltandroom/ui/launcher_screen/launcher_screen.kt
@@ -1,18 +1,18 @@
 package com.nick.samplecomposewithhiltandroom.ui.launcher_screen
 
-import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.nick.samplecomposewithhiltandroom.R
 import com.nick.samplecomposewithhiltandroom.utils.screen_routes.Screens.SHIPS_SCREEN
@@ -20,9 +20,6 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun LauncherScreen(navController: NavController) {
-    val scale = remember {
-        Animatable(0f)
-    }
     LaunchedEffect(key1 = true) {
         delay(2000L)
         navController.navigate(SHIPS_SCREEN)
@@ -36,7 +33,9 @@ fun LauncherScreen(navController: NavController) {
         Image(
             painter = painterResource(id = R.drawable.ic_baseline_image_24),
             contentDescription = null,
-            modifier = Modifier.scale(scale.value)
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
         )
     }
 }
