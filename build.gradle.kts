42ec89473d0dd755bf311fed9f23293823c7c725
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-04-05 23:01:52 +0300

    update some dependencies libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     dependencies {
-        val hiltVersion by extra("2.51")
+        val hiltVersion by extra("2.51.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -8,9 +8,9 @@ plugins {
     id("com.android.application") version "8.3.1" apply false
     id("com.android.library") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
