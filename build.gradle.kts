0fb2c357cf5889fff060a3ad64d915658901dd68
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-06-16 19:01:53 +0300

    update Gradle and update the libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.1" apply false
-    id("com.android.library") version "8.4.1" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("com.android.library") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
 }
