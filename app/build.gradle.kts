4e03da136905d3d2168a5cb38c356038967f2ed2
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-11-03 00:06:39 +0200

    update all libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.nick.samplecomposewithhiltandroom"
-        minSdk = 27
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
