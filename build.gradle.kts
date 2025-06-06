cf944aa324ee6d02bbe32ec41df1bd45d7a3a1e5
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-06-06 23:15:54 +0300

    update Kotlin and KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
 plugins {
     id("com.android.application") version "8.10.1" apply false
     id("com.android.library") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
 }
 
 tasks.register("clean", Delete::class) {
