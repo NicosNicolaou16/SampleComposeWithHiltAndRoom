0427bbab1ccdff3e281b88a1bfdf7f09390c2186
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-09-25 20:14:42 +0300

    update all libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("kotlin-kapt")
     id("com.google.devtools.ksp")
     id("dagger.hilt.android.plugin")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20"
 }
 
 android {
