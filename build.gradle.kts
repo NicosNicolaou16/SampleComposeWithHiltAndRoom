e689a1a91360459a8c61ceceddf7712593824b6c
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-02-18 21:03:30 +0200

    update KSP version to 2.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("com.android.application") version "9.0.1" apply false
     id("com.android.library") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.10" apply false
-    id("com.google.devtools.ksp") version "2.3.5" apply false
+    id("com.google.devtools.ksp") version "2.3.6" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10" apply false
 }
 
