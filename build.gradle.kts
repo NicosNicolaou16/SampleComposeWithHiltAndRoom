d543e34914c8b603c71f743b28d521f78b9ef511
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-02-18 20:52:00 +0200

    update Gradle 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.13.2" apply false
-    id("com.android.library") version "8.13.2" apply false
+    id("com.android.application") version "9.0.1" apply false
+    id("com.android.library") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("com.google.devtools.ksp") version "2.3.4" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
